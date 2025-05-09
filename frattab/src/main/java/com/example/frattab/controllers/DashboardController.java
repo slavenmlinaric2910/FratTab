dcc0deaa6ef44b77c96f47196bd4f0beb5027b09
Damjan Mlinaric <damjan.mlinaric@finlex.de>
2025-05-09 17:34:08 +0200

    UI dashboard and member page

--- frattab/src/main/java/com/example/frattab/controllers/DashboardController.java
+++ frattab/src/main/java/com/example/frattab/controllers/DashboardController.java
@@ -1,13 +1,22 @@
 package com.example.frattab.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import com.example.frattab.models.Member;
 
 @Controller
 public class DashboardController {
     @GetMapping("/dashboard")
     public String getDashboardPage() {
         return "./dashboard";
+    }
+
+    @GetMapping("/dashboard/members")
+    public String getMembersPage(Model model) {
+        model.addAttribute("member", new Member());
+        return "./members";
     }
 
 }
---
+++ frattab/src/main/java/com/example/frattab/models/Member.java
@@ -0,0 +1,12 @@
+package com.example.frattab.models;
+
+import lombok.Data;
+
+@Data
+public class Member {
+    private int id;
+    private String firstName;
+    private String lastName;
+    private String nickName;
+    private String email;
+}
