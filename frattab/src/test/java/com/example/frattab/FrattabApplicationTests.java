f7e2e13404fddc16a20ca7f1188512e600832c8d
Slaven Mlinaric <slaven.mlinaric@finlex.de>
2025-05-09 14:14:34 +0200

    Project base - without code.

---
+++ frattab/src/test/java/com/example/frattab/FrattabApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.frattab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FrattabApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
